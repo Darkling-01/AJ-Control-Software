5e72a589738a2c023ac76c9e26d09d9a948c8f6b
Alejandro Barragan <57509594+Darkling-01@users.noreply.github.com>
2025-05-15 22:01:33 -0700

    Update examples.cpp

--- src/examples/examples.cpp
+++ src/examples/examples.cpp
@@ -5,3 +5,13 @@ void Examples::led_Blink()
 {
 std::cout << "// Code goes here" << std::endl;
 }
+
+void Examples::servo_motor()
+{
+    // code here
+}
+
+void Examples::led_switch()
+{
+    // code here
+}
